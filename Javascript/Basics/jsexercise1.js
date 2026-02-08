00bd889dc9db67fc11f85f40c95243922e41698e
faisalndottijoyerima <fnyerima.reg@buk.edu.ng>
2026-02-08 18:09:41 +0100

    i write my profile

---
+++ Javascript/Basics/jsexercise1.js
@@ -0,0 +1,22 @@
+let name = "maimuna";
+let age = "25";
+let city = "yola";
+let isStudent = "true";
+let person = {
+    name: name,
+    age: age,
+    city: city,
+
+};
+
+hobbies = [
+    "coding", "reading", "travelling"
+];
+
+console.log("name:", name);
+console.log("age:", age,);
+console.log( "city:", city,);
+console.log("isStudent", isStudent);
+console.log("city:", city);
+console.log("person:", person);
+console.log("hobbies:", hobbies);
